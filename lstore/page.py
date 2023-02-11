332be78b0668122c4e80467e562e0bbfa0c74f52
HugoLin-16 <76938794+HugoLin-16@users.noreply.github.com>
2023-02-11 14:20:48 -0800

    Update page.py
    still need to work on page range

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,15 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
-
+        if self.num_records < 512:
+            return True
+        else:
+            return False
+        
     def write(self, value):
+        begin = self.num_records * 8
+        end = (self.num_records + 1) * 8
+        self.data[begin:end] = value.to_bytes(8, 'little')
         self.num_records += 1
         pass
 

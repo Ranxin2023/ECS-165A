68ae7a4bd11c1ef96c8d2d063c36de35b22e78db
Ranxin2023 <60165560+Ranxin2023@users.noreply.github.com>
2023-02-26 22:21:40 -0800

    finish index, still working on bTree

--- lstore/index.py
+++ lstore/index.py
@@ -3,13 +3,14 @@ A data strucutre holding indices for various columns of a table. Key column shou
 """
 
 from lstore.page import Page
-
+from lstore.btree import BTree
 class Index:
 
     def __init__(self, table):
         # One index for each table. All our empty initially.
-        self.indices = [None] *  table.num_columns
-        
+        self.indices = [list() for i in range(table.total_num_columns)]
+        self.column_num = dict()
+        self.table = table
         pass
 
     """
@@ -17,14 +18,25 @@ class Index:
     """
 
     def locate(self, column, value):
-        pass
+        return_list = []
+        column_values = sorted(self.indices[column])
+        for column_rid in column_values:
+            if column_rid[0] == value:
+                return_list.append(column_rid[1])
+        return return_list
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
-        pass
+        return_list = []
+        column_values = sorted(self.indices[column])
+
+        for column_rid in column_values:
+            if column_rid[0] >= begin and column_rid[0] <=end:
+                return_list.append(column_rid[1])
+        return return_list
 
     """
     # optional: Create index on specific column
@@ -39,3 +51,14 @@ class Index:
 
     def drop_index(self, column_number):
         pass
+
+    def push_index(self, columns):
+        for i in range(1, self.table.total_num_columns):
+            self.indices[i].append((columns[i], columns[0]))
+            self.column_num[columns[i]] = i
+    '''
+    def lower_bound(self, list):
+        low, up=0, len(list)-1
+        while low<up:
+            mid=(low+up)//2
+    '''
